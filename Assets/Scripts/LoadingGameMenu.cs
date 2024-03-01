97f21856a4198e9144c72df44f7402bb91e0aa59
mnavindug <mmnavindug@gmail.com>
2024-03-01 15:32:09 +0530

    added a starting screen that transitions to the game menu after authentication

---
+++ Assets/Scripts/LoadingGameMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingGameMenu : MonoBehaviour
+{
+    [SerializeField]
+    public Image progressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(CheckStatus());
+    }
+
+     IEnumerator CheckStatus() 
+    {
+        float progress = 0f;
+        progressBar.fillAmount = progress;
+
+         while (progress < 1f)
+        {
+            progress += Time.deltaTime / 3; 
+            progressBar.fillAmount = progress;
+            yield return null;
+        }
+       
+       if (APIRequestHandler.IsAuthenticated())
+        {
+            // Load game scene
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene(2);
+            Debug.Log("Game menu loads.");
+        }
+        else
+        {
+            Debug.LogError("Authentication failed!");
+        }
+
+       
+    }
+
+    
+
+}
